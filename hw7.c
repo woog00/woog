e65e648d760102f974d50cf22a7fdf086a99d371
woog00 <dnrud0820@naver.com>
2024-11-05 16:59:10 +0900

    202410834_김우경
    개념확인과제입니다.

---
+++ hw7.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int arr1[6] = {1,2,3,4,5,6};
+	int arr2[6] = { 7,8,9,10,11,12 };
+	int*ptr1 = &arr1;
+	int* ptr2 = &arr2;
+	int i,temp;
+	printf("arr1: ");
+	for (i = 0; i < 6; i++) {
+
+		printf("%d ", arr1[i]);
+	}
+	printf("\n");
+	printf("arr2: ");
+	for (i = 0; i < 6; i++) {
+
+		printf("%d ", arr2[i]);
+	}
+	printf("\n\nafter swap\n");
+	for (i = 0; i < 6; i++)
+	{
+		temp = *ptr1;
+		*ptr1 = *ptr2;
+		*ptr2 = temp;
+		ptr1++;
+		ptr2++;
+		
+	}
+	printf("arr1: ");
+	for (i = 0; i < 6; i++) {
+
+		printf("%d ", arr1[i]);
+	}
+	printf("\n");
+	printf("arr2: ");
+	for (i = 0; i < 6; i++) {
+
+		printf("%d ", arr2[i]);
+	}
+	
+	
+	return 0;
+}
